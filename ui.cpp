c6843c09fa0d7a7d6374e556a851455b134e8562
Andrew <ordinandrej@gmail.com>
2024-05-04 04:39:33 +0300

    smth

--- ui.cpp
+++ ui.cpp
@@ -6,5 +6,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 bool App::OnInit() {
     MainFrame* mainframe = new MainFrame("test");
     mainframe->Show();
+    mainframe->Center();
+
     return true;
 }
