25984dd2d80a3bac0d51cf5aece478225175cad7
Andrew <ordinandrej@gmail.com>
2024-05-09 02:04:38 +0300

    smthing

--- ui.cpp
+++ ui.cpp
@@ -1,4 +1,5 @@
 #include <wx/listctrl.h>
+#include <vector>
 #include <wx/wx.h>
 #include <wx/toolbar.h>
 #include "ui.h"
@@ -34,6 +35,8 @@ bool MyApp::OnInit() {
 }
 MyToolBar::MyToolBar(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size){
     ToolBar = new wxToolBar(parent,id,pos,size);
+    BookSelect = new BookSelectFrame("Добавление книги");
+
     ToolBar->AddTool(ATool, "NEW", wxArtProvider::GetBitmap("wxART_PLUS"));
     ToolBar->AddTool(RTool, "NEW", wxArtProvider::GetBitmap("wxART_MINUS"));
     ToolBar->Bind(wxEVT_TOOL, &MyToolBar::Actions, this);
@@ -50,39 +53,45 @@ BookSelectFrame::BookSelectFrame(const wxString& title)
     auto SizerText = new wxBoxSizer(wxVERTICAL);
     auto SizerButtons = new wxBoxSizer(wxHORIZONTAL);
 
-    auto NameLabel = new wxStaticText(this,wxID_ANY,wxT("Название Книги"));
+    auto NameLabel = new wxStaticText(this,wxID_ANY,wxT("Название Книги:"));
     auto NameTextCtrl = new wxTextCtrl(this,wxID_ANY);
-    SizerText->Add(NameLabel);
-    SizerText->Add(NameTextCtrl);
 
-    auto AutherLabel = new wxStaticText(this,wxID_ANY,wxT("Автор"));
+    auto AutherLabel = new wxStaticText(this,wxID_ANY,wxT("Автор:"));
     auto AutherTextCtrl = new wxTextCtrl(this,wxID_ANY);
-    SizerText->Add(AutherLabel);
-    SizerText->Add(AutherTextCtrl);
 
-    auto DateLabel = new wxStaticText(this,wxID_ANY,wxT("Дата издания"));
+    auto DateLabel = new wxStaticText(this,wxID_ANY,wxT("Дата издания:"));
     auto DateTextCtrl = new wxTextCtrl(this,wxID_ANY);
-    SizerText->Add(DateLabel);
-    SizerText->Add(DateTextCtrl);
 
-    auto PageLabel = new wxStaticText(this,wxID_ANY,wxT("Кол-во страниц"));
+    auto PageLabel = new wxStaticText(this,wxID_ANY,wxT("Кол-во страниц:"));
     auto PageTextCtrl = new wxTextCtrl(this,wxID_ANY);
-    SizerText->Add(PageLabel);
-    SizerText->Add(PageTextCtrl);
+    std::vector<wxWindow*> fromItems = {
+        NameLabel,
+        NameTextCtrl,
+        AutherLabel,
+        AutherTextCtrl,
+        DateLabel,
+        DateTextCtrl,
+        PageLabel,
+        PageTextCtrl};
+    for (auto Item:fromItems) {
+        SizerText->Add(Item,0,wxEXPAND|wxALL,FromDIP(3));
+    }
 
     auto addButton = new wxButton(this,wxID_ANY,wxT("Добавить книгу"),wxDefaultPosition,wxDefaultSize);
-    SizerButtons->Add(addButton);
-    auto rmButton = new wxButton(this,wxID_ANY,wxT("Удалить книгу"),wxDefaultPosition,wxDefaultSize);
-    SizerButtons->Add(rmButton);
+    SizerButtons->Add(addButton,1,wxEXPAND|wxALL,FromDIP(3));
+    auto rmButton = new wxButton(this,wxID_ANY,wxT("Закрыть"),wxDefaultPosition,wxDefaultSize);
+    rmButton->Bind(wxEVT_BUTTON,[this](wxCommandEvent& event){
+        this->Show(false);});
+    SizerButtons->Add(rmButton,1,wxEXPAND|wxALL,FromDIP(3));
 
-    SizerMain->Add(SizerText);
-    SizerMain->Add(SizerButtons);
-    this->SetSizer(SizerMain);
+    SizerMain->Add(SizerText,1,wxALIGN_CENTER | wxRIGHT|wxTOP,FromDIP(10));
+    SizerMain->Add(SizerButtons,0,wxALIGN_CENTER | wxLEFT|wxRIGHT);
+    this->SetSizerAndFit(SizerMain);
 }
 void MyToolBar::Actions(wxCommandEvent& event){
     switch(event.GetId()) {
         case ATool:
-            this->BookSelect->Show();
+            this->BookSelect->Show(true);
 
     }
 }
